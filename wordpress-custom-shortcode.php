a33b2711fac3e50c807bc879cd4f685a837e03b7
FarrasPratama <pratama007fr@gmail.com>
2024-01-11 17:03:19 +0700

    feat: change plugin parsing method

--- wordpress-custom-shortcode.php
+++ wordpress-custom-shortcode.php
@@ -2,37 +2,37 @@
 /**
  * Plugin Name: Custom Banner Shortcode
  * Description: A custom plugin for embedding a banner with dynamic content.
- * Version: 1.4
+ * Version: 1.7
  * Author: Farras Pratama
  */
 
-function custom_banner_shortcode($atts) {
-    // Default values
-    $atts = shortcode_atts(
-        array(
-            'banner_copy' => 'Default Banner Copy',
-            'button_url' => '#',
-            'button_label' => 'Default Button Label',
-        ),
-        $atts,
-        'custom_banner'
-    );
-
-    // Generate the HTML for the iframe with dynamic content
-    $iframe_html = '<iframe id="customBannerIframe" src="https://icn-dev.github.io/coinvestasi-button-shortcode/banner.html" width="100%" height="auto" frameborder="0"></iframe>';
-
-    // Generate the JavaScript to directly set parameters
-    $script = '<script>
-                  document.addEventListener("DOMContentLoaded", function() {
-                      var iframe = document.getElementById("customBannerIframe");
-                      iframe.contentWindow.postMessage({
-                          bannerCopy: "' . esc_js($atts['banner_copy']) . '",
-                          buttonURL: "' . esc_js($atts['button_url']) . '",
-                          buttonLabel: "' . esc_js($atts['button_label']) . '"
-                      }, "https://icn-dev.github.io");
-                  });
-              </script>';
-
-    return $iframe_html . $script;
-}
-add_shortcode('custom_banner', 'custom_banner_shortcode');
+ function custom_banner_shortcode($atts) {
+     // Extract shortcode attributes
+     $atts = shortcode_atts(
+         array(
+             'banner_copy' => 'Default Banner Copy',
+             'button_url' => 'https://coinvestasi.com/',
+             'button_label' => 'Default Button Label',
+         ),
+         $atts,
+         'custom_banner'
+     );
+ 
+     // Encode attribute values for URL
+     $banner_copy = rawurlencode($atts['banner_copy']);
+     $button_url = rawurlencode($atts['button_url']);
+     $button_label = rawurlencode($atts['button_label']);
+ 
+     // Build the dynamic URL
+     $dynamic_url = "https://icn-dev.github.io/coinvestasi-button-shortcode/banner.html?banner_copy=$banner_copy&button_url=$button_url&button_label=$button_label";
+ 
+     // Output the HTML with the dynamic URL
+     $output = "<iframe src='$dynamic_url' style='width: 100%; height: 200px; border: none;'></iframe>";
+ 
+     return $output;
+ }
+ 
+ // Register the shortcode
+ add_shortcode('custom_banner', 'custom_banner_shortcode');
+ ?>
+ 

6449b47f8296589fa33147c9aace9113c22c87cf
FarrasPratama <pratama007fr@gmail.com>
2024-01-11 16:35:30 +0700

    feat: change plugin function

--- wordpress-custom-shortcode.php
+++ wordpress-custom-shortcode.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Custom Banner Shortcode
  * Description: A custom plugin for embedding a banner with dynamic content.
- * Version: 1.3
+ * Version: 1.4
  * Author: Farras Pratama
  */
 
@@ -21,16 +21,16 @@ function custom_banner_shortcode($atts) {
     // Generate the HTML for the iframe with dynamic content
     $iframe_html = '<iframe id="customBannerIframe" src="https://icn-dev.github.io/coinvestasi-button-shortcode/banner.html" width="100%" height="auto" frameborder="0"></iframe>';
 
-    // Generate the JavaScript to update the iframe content
+    // Generate the JavaScript to directly set parameters
     $script = '<script>
-                  var iframe = document.getElementById("customBannerIframe");
-                  iframe.onload = function() {
+                  document.addEventListener("DOMContentLoaded", function() {
+                      var iframe = document.getElementById("customBannerIframe");
                       iframe.contentWindow.postMessage({
                           bannerCopy: "' . esc_js($atts['banner_copy']) . '",
                           buttonURL: "' . esc_js($atts['button_url']) . '",
                           buttonLabel: "' . esc_js($atts['button_label']) . '"
-                      }, "*");
-                  };
+                      }, "https://icn-dev.github.io");
+                  });
               </script>';
 
     return $iframe_html . $script;
